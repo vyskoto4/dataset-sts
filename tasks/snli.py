930255ac019b8a7b99e3b84f8600cf0bff1a8999
Tomas Vyskocil <tomasvysk@gmail.com>
2016-05-16 23:03:05 +0200

    Started implementing model from snli paper

--- tasks/snli.py
+++ tasks/snli.py
@@ -8,6 +8,9 @@ Training example:
 
 Before training, you must however run:
    tools/snli_preprocess.py data/snli/snli_1.0_train.jsonl data/snli/snli_1.0_dev.jsonl data/snli/snli_1.0_test.jsonl data/snli/snli_1.0_train.pickle  data/snli/snli_1.0_dev.pickle data/snli/snli_1.0_test.pickle data/snli/v1-vocab.pickle
+
+current experimental model implementation based on https://github.com/shyamupa/snli-entailment/blob/master/amodel.py
+
 """
 
 from __future__ import print_function
@@ -15,18 +18,40 @@ from __future__ import division
 
 
 from keras.callbacks import EarlyStopping, ModelCheckpoint
-from keras.layers.core import Activation, Dropout
+from keras.layers import GRU
+from keras.layers.core import Activation, Dropout, Lambda, Dense, RepeatVector, TimeDistributedDense, Reshape
 from keras.models import Graph
+from keras import backend as K
 
 import pickle
 import pysts.eval as ev
 import numpy as np
 
-
+from keras.regularizers import l2
 from pysts.kerasts import graph_input_anssel
 import pysts.kerasts.blocks as B
 
 from .anssel import AbstractTask
+
+
+# 60 like spad
+xmaxlen=60;
+
+def get_Y(X):
+    return X[:, :xmaxlen, :]  # get first xmaxlen elem from time dim
+
+def get_R(X):
+    Y, alpha = X.values()  # Y should be (None,L,k) and alpha should be (None,L,1) and ans should be (None, k,1)
+    tmp=K.permute_dimensions(Y,(0,)+(2,1))  # copied from permute layer, Now Y is (None,k,L) and alpha is always (None,L,1)
+    ans=K.T.batched_dot(tmp,alpha)
+    return ans
+
+def get_H_n(X):
+    ans=X[:, -1, :]  # get last element from time dim
+    return ans
+
+
+
 
 class SnliTask(AbstractTask):
     def __init__(self):
@@ -78,29 +103,53 @@ class SnliTask(AbstractTask):
             res.append(ev.eval_snli(ypred, gr['score'], fname))
         return tuple(res)
 
-    def prep_model(self,module_prep_model):
+
+    def prep_model(self, module_prep_model):
         # Input embedding and encoding
         model = Graph()
+        sdim=2
         N = B.embedding(model, self.emb, self.vocab, self.s0pad, self.s1pad,
                         self.c['inp_e_dropout'], self.c['inp_w_dropout'], add_flags=self.c['e_add_flags'])
-        # Sentence-aggregate embeddings
-        final_outputs = module_prep_model(model, N, self.s0pad, self.s1pad, self.c)
-        # Measurement
-        kwargs = dict()
-        if self.c['ptscorer'] == B.mlp_ptscorer:
-            kwargs['sum_mode'] = self.c['mlpsum']
-        model.add_node(name='scoreS0', input=self.c['ptscorer'](model, final_outputs, self.c['Ddim'], N, self.c['l2reg'],pfx="out0", **kwargs),
-                       layer=Activation('sigmoid'))
 
-        model.add_node(name='scoreS1', input=self.c['ptscorer'](model, final_outputs, self.c['Ddim'], N, self.c['l2reg'],pfx="out1", **kwargs),
-                       layer=Activation('sigmoid'))
+        emb_outs=['e0_', 'e1_']
+        model.add_node(name='backward', inputs=[emb_outs], merge_mode='concat', layer=Activation('linear'))
+        model.add_node(name='rnn0', inputs='emb_merge', outputs=['e0s'],
+                              layer=GRU(input_dim=N, input_length=2*self.spad,
+                                        init='glorot_uniform', activation='tanh',
+                                        return_sequences=True,go_backwards=True))
+        model.add_node(name='forward', inputs='emb_merge', outputs=['e0s'],
+                              layer=GRU(input_dim=N, input_length=2*self.spad,
+                                        init='glorot_uniform', activation='tanh',
+                                        return_sequences=True))
+        #model.add_node(GRU(opts.lstm_units, return_sequences=True), name='forward', input='emb_merge')
+        #model.add_node(GRU(opts.lstm_units, return_sequences=True, go_backwards=True), name='backward',  input='emb_merge')
 
-        model.add_node(name='scoreS2', input=self.c['ptscorer'](model, final_outputs, self.c['Ddim'], N, self.c['l2reg'],pfx="out2", **kwargs),
-                       layer=Activation('sigmoid'))
 
-        model.add_node(name='scoreV', inputs=['scoreS0', 'scoreS1', 'scoreS2'], merge_mode='concat', layer=Activation('softmax'))
+        # FIXME! the code is dirty...
+        model.add_node(Dropout(0.1), name='dropout', inputs=['forward','backward'])
 
-        model.add_output(name='score', input='scoreV')
+
+        k=N # glove dims
+        L=self.spad # L = number of words of premise
+        model.add_node(Lambda(get_H_n, output_shape=(k,)), name='h_n', input='dropout')
+
+
+        model.add_node(Lambda(get_Y, output_shape=(L, k)), name='Y', input='dropout')
+        # model.add_node(SliceAtLength((None,N,k),L), name='Y', input='dropout')
+        model.add_node(Dense(k,W_regularizer=l2(0.01)),name='Wh_n', input='h_n')
+        model.add_node(RepeatVector(L), name='Wh_n_cross_e', input='Wh_n')
+        model.add_node(TimeDistributedDense(k,W_regularizer=l2(0.01)), name='WY', input='Y')
+        model.add_node(Activation('tanh'), name='M', inputs=['Wh_n_cross_e', 'WY'], merge_mode='sum')
+        model.add_node(TimeDistributedDense(1,activation='softmax'), name='alpha', input='M')
+        model.add_node(Lambda(get_R, output_shape=(k,1)), name='_r', inputs=['Y','alpha'], merge_mode='join')
+        model.add_node(Reshape((k,)),name='r', input='_r')
+        model.add_node(Dense(k,W_regularizer=l2(0.01)), name='Wr', input='r')
+        model.add_node(Dense(k,W_regularizer=l2(0.01)), name='Wh', input='h_n')
+        model.add_node(Activation('tanh'), name='h_star', inputs=['Wr', 'Wh'], merge_mode='sum')
+
+        model.add_node(Dense(3, activation='softmax'), name='out', input='h_star')
+        model.add_output(name='score', input='out')
+
         return model
 
 
